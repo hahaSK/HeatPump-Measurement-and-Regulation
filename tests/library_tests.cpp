b25a1c5242b2e14caf4b9d16cdc96203d1c8830f
hahaSK <xlahvi00@vutbr.cz>
2019-03-05 11:43:10 +0200

    Added Google tests
    Signed-off-by: hahaSK <xlahvi00@vutbr.cz>

---
+++ tests/library_tests.cpp
@@ -0,0 +1,24 @@
+//
+// Created by Juraj Lahvička on 5.3.2019.
+//
+
+#include "googletest-master/googletest/include/gtest/gtest.h"
+
+#include "googletest-master/googlemock/include/gmock/gmock.h"
+#include "../src/library.h"
+
+
+TEST(CalculateCOP, Basic)
+{
+  ASSERT_DOUBLE_EQ(CalculateCOP(100, 20), 5);
+}
+
+TEST(CalculatePhe, DeltaT)
+{
+  ASSERT_DOUBLE_EQ(CalculatePhe(150, 1.005, 5), 753.75);
+}
+
+TEST(CalculatePhe, Temperatures)
+{
+  ASSERT_DOUBLE_EQ(CalculatePhe(150, 1.005, 20, 15), 753.75);
+}
