26c8a7768de3834b04648555176bf8493051d533
hahaSK <xlahvi00@vutbr.cz>
2019-03-05 11:49:27 +0200

    Comments moved to header file
    Signed-off-by: hahaSK <xlahvi00@vutbr.cz>

--- src/library.h
+++ src/library.h
@@ -1,6 +1,31 @@
 #ifndef HEATPUMP_MEASUREMENT_AND_REGULATION_LIBRARY_H
 #define HEATPUMP_MEASUREMENT_AND_REGULATION_LIBRARY_H
 
-void hello();
+/**
+ * Calculates the COP (Coefficient of Performance)
+ * @param Phe Heat Capacity [W]
+ * @param Pe Electrical Capacity [W]
+ * @return The COP of the system [-]
+ */
+double CalculateCOP(double Phe, double Pe);
+
+/**
+ * Calculates the Heat Capacity of given substance
+ * @param Qm mass flow rate [kg/s]
+ * @param Cp thermal capacity of given substance [J/KgK]
+ * @param DeltaT temperature difference [K]
+ * @return <strong>Phe</strong> - Heat Capacity [W]
+ */
+double CalculatePhe(double Qm, double Cp, double DeltaT);
+
+/**
+ * Calculates the Heat Capacity ofc given substance
+ * @param Qm mass flow rate [kg/s]
+ * @param Cp thermal capacity of given substance [J/KgK]
+ * @param T1 temperature [T] or [°C]
+ * @param T2 temperature [T] or [°C]
+ * @return <strong>Phe</strong> - Heat Capacity [W]
+ */
+double CalculatePhe(double Qm, double Cp, double T1, double T2);
 
 #endif
