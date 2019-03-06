08f583de64083af4b06bac9d5ec890c68d883d54
hahaSK <xlahvi00@vutbr.cz>
2019-03-06 13:10:06 +0200

    Added gui
    Signed-off-by: hahaSK <xlahvi00@vutbr.cz>

---
+++ gui/MainGUI/MainGUI/COPandPCalcLib.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace MainGUI
+{
+    public static class COPandPCalcLib
+    {       
+        [DllImport("COPandPCalc", CallingConvention = CallingConvention.Cdecl)]
+        private static extern double CalculateCOP(double Phe, double Pe);
+        [DllImport("COPandPCalc", CallingConvention = CallingConvention.Cdecl)]
+        private static extern double CalculatePhe(double Qm, double Cp, double DeltaT);
+        [DllImport("COPandPCalc", CallingConvention = CallingConvention.Cdecl)]
+        private static extern double CalculatePhe(double Qm, double Cp, double T1, double T2);
+        [DllImport("COPandPCalc", CallingConvention = CallingConvention.Cdecl)]
+        private static extern double CalculatePe(double U, double I);
+
+        /// <summary>
+        /// Calculates the COP (Coefficient of Performance)
+        /// </summary>
+        /// <param name="Phe">Phe Heat Capacity [W]</param>
+        /// <param name="Pe">Pe Electrical Capacity [W]</param>
+        /// <returns>The COP of the system [-]. <see cref="T:null" /> if DLL is not found.</returns>
+        public static double? CalcCOP(double Phe, double Pe)
+        {
+            try
+            {
+                return CalculateCOP(Phe, Pe);
+            }
+            catch (DllNotFoundException dllNotFound)
+            {
+                ErrorPrinting.PrintError(dllNotFound.Message);
+                return null;
+            }
+        }
+
+        /// <summary>
+        /// Calculates the Heat Capacity of given substance
+        /// </summary>
+        /// <param name="Qm">mass flow rate [kg/s]</param>
+        /// <param name="Cp">specific thermal capacity of given substance [J/KgK]</param>
+        /// <param name="DeltaT">temperature difference [K]</param>
+        /// <returns>Heat Capacity [W]. <see cref="T:null" /> if DLL is not found.</returns>
+        public static double? CalcPhe(double Qm, double Cp, double DeltaT)
+        {
+            try
+            {
+                return CalculatePhe(Qm, Cp, DeltaT);
+            }
+            catch (DllNotFoundException dllNotFoundException)
+            {
+                ErrorPrinting.PrintError(dllNotFoundException.Message);
+                return null;
+            }
+        }
+
+        /// <summary>
+        /// Calculates the Heat Capacity of given substance
+        /// </summary>
+        /// <param name="Qm">mass flow rate [kg/s]</param>
+        /// <param name="Cp">specific thermal capacity of given substance [J/KgK]</param>
+        /// <param name="T1">temperature [T] or [°C]</param>
+        /// <param name="T2">temperature [T] or [°C]</param>
+        /// <returns>- Heat Capacity [W]. <see cref="T:null" /> if DLL is not found.</returns>
+        public static double? CalcPhe(double Qm, double Cp, double T1, double T2)
+        {
+            try
+            {
+                return CalculatePhe(Qm, Cp, T1, T2);
+            }
+            catch (DllNotFoundException dllNotFoundException)
+            {
+                ErrorPrinting.PrintError(dllNotFoundException.Message);
+                return null;
+            }
+        }
+
+        /// <summary>
+        /// Calculates the (consumed) Electrical capacity
+        /// </summary>
+        /// <param name="U">Voltage</param>
+        /// <param name="I">Current</param>
+        /// <returns>Electrical capacity. <see cref="T:null" /> if DLL is not found.</returns>
+        public static double? CalcPe(double U, double I)
+        {
+            try
+            {
+                return CalculatePe(U, I);
+
+            }
+            catch (DllNotFoundException dllNotFound)
+            {
+                ErrorPrinting.PrintError(dllNotFound.Message);
+                return null;
+            }
+        }
+    }
+}
---
+++ gui/MainGUI/MainGUI/MainWindow.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows;
+
+namespace MainGUI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private CalculateCOPandP calculateCoPandP = new CalculateCOPandP();
+        private GUIChecks guiChecks = new GUIChecks();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Calc_Click(object sender, RoutedEventArgs e)
+        {
+            if (!guiChecks.TryGetValue(PheTextBox, out double Phe) || !guiChecks.TryGetValue(PeTextBox, out double Pe))
+                return;
+
+            ResultTextBox.Text = calculateCoPandP.CalculateCOP(Phe, Pe).ToString();
+        }
+    }
+}
