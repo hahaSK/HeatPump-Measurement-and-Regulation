f594ad0d01cfbd0749554352ec3f940c5b4f01c4
hahaSK <xlahvi00@vutbr.cz>
2019-03-30 19:55:03 +0200

    Modified Air density tests
    Signed-off-by: hahaSK <xlahvi00@vutbr.cz>

--- tests/AirRho_tests.cpp
+++ tests/AirRho_tests.cpp
@@ -9,7 +9,9 @@
 
 TEST(GetAirRho, Basic)
 {
-  ASSERT_DOUBLE_EQ(GetAirRho(0, 0, 15), 1.224977);
-  ASSERT_DOUBLE_EQ(GetAirRho(150, 32, 40), 1.128069);
-  ASSERT_DOUBLE_EQ(GetAirRho(0, 35, 20), 1.200407);
+  float delta{0.000001};
+  ASSERT_NEAR(GetAirRho(0, 0, 15), 1.22497705587732, delta);
+  ASSERT_NEAR(GetAirRho(150, 40, 32), 1.12806909389719, delta);
+  ASSERT_NEAR(GetAirRho(0, 35, 20), 1.20040787096485, delta);
+  ASSERT_NEAR(GetAirRho(200, 80, 13), 1.19899065751873, delta);
 }
