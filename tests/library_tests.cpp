0481b9eb1afa0ee6c5c69310ab28d203cf05fb5a
hahaSK <xlahvi00@vutbr.cz>
2019-05-12 23:43:57 +0300

    Added more tests
    Signed-off-by: hahaSK <xlahvi00@vutbr.cz>

--- tests/library_tests.cpp
+++ tests/library_tests.cpp
@@ -11,19 +11,23 @@
 TEST(CalculateCOP, Basic)
 {
   ASSERT_DOUBLE_EQ(CalculateCOP(100, 20), 5);
+  ASSERT_DOUBLE_EQ(CalculateCOP(20000, 8000), 2.5);
 }
 
 TEST(CalculatePhe, DeltaT)
 {
   ASSERT_DOUBLE_EQ(CalculatePhe(150, 1.005, 5), 753.75);
+  ASSERT_DOUBLE_EQ(CalculatePhe(300, 1.003, 10), 3009);
 }
 
 TEST(CalculatePhe, Temperatures)
 {
   ASSERT_DOUBLE_EQ(CalculatePhe(150, 1.005, 20, 15), 753.75);
+  ASSERT_DOUBLE_EQ(CalculatePhe(300, 1.003, 20.2, 10.2), 3009);
 }
 
 TEST(CalculatePe, Basic)
 {
   ASSERT_DOUBLE_EQ(CalculatePe(230, 2), 460);
+  ASSERT_DOUBLE_EQ(CalculatePe(230, 5), 1150);
 }
